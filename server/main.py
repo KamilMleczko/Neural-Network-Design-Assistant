500436f8ec1e2a7fd3aae1b4598d4a13829935cc
KamilMleczko <kamil.mleczko.4242@gmail.com>
2025-09-21 16:01:37 +0200

    feat(frontend): biome linter, formatter, pre-commit

--- server/main.py
+++ server/main.py
@@ -5,12 +5,12 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+  return {"Hello": "World"}
 
 
 def main():
-    print("Hello from backend!")
+  print("Hello from backend!")
 
 
 if __name__ == "__main__":
-    main()
+  main()
